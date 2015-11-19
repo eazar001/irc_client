ff5c35b1a1758bc667621ab38ad1e305baf2653a
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-11-18 22:55:28 -0800

    Release v1.1.0

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(irc_client).
 title('Low level interface to IRC clients').
-version('1.0.0').
+version('1.1.0').
 author('Ebrahim Azarisooreh', 'ebrahim.azarisooreh@gmail.com').
 home('https://github.com/eazar001/irc_client').
 keywords([irc,chat]).
-download('https://github.com/eazar001/irc_client/archive/v1.0.0.zip').
+download('https://github.com/eazar001/irc_client/archive/v1.1.0.zip').
