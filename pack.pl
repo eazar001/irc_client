5529d145794611f1af7a4c77b11303f167f4715c
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-11-23 18:43:26 -0800

    Updating pack metadata.

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(irc_client).
 title('Low level interface to IRC clients').
-version('1.1.1').
+version('1.1.2').
 author('Ebrahim Azarisooreh', 'ebrahim.azarisooreh@gmail.com').
 home('https://github.com/eazar001/irc_client').
 keywords([irc,chat]).
-download('https://github.com/eazar001/irc_client/archive/v1.1.1.zip').
+download('https://github.com/eazar001/irc_client/archive/v1.1.2.zip').
