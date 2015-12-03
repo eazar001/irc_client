5b9d64637ee7f708770501a26eb39e461830ce33
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2015-12-03 02:42:12 -0800

    Removing obsolete timer/2 fact.

--- prolog/info.pl
+++ prolog/info.pl
@@ -7,18 +7,15 @@
       ,handle_server/2
       ,connection/7
       ,min_msg_len/2
-      ,c_specs/6
-      ,timer/2 ]).
+      ,c_specs/6 ]).
 
 :- dynamic known/2.
 :- dynamic get_irc_stream/2.
 :- dynamic get_irc_write_stream/2.
 :- dynamic connection/7.
 :- dynamic c_specs/6.
-:- dynamic timer/2.
 
 :- dynamic get_irc_server/2.
 :- dynamic min_msg_len/2.
 
 :- dynamic handle_server/2.
-
